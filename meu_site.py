f2a4353baedeb15c700d6df03de46c317bbfb9de
Stephan Morais <stephan.rpa@gmail.com>
2024-02-01 13:13:52 -0300

    Changes to be committed: 	modified:   meu_site.py 	new file:   solicitacoesCadastro.json 	modified:   templates/filiacao.html

--- meu_site.py
+++ meu_site.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request, flash
+
 
 app=Flask(__name__)
 
@@ -23,6 +24,35 @@ def filiacao():
 def loja():
     return render_template("loja.html")
 
+@app.route("/cadastrar", methods=['POST'])
+def cadastrar():
+    user=[]
+    
+    nome = request.form.get('nome')
+    guilda = request.form.get('guilda')
+    profissao = request.form.get('profissao')
+    situacaoEconomica = request.form.get('RENDA')
+    corPrincipal = request.form.get('cor')
+    comprovante = request.form.get('CIENCIA')
+    user=[{
+        "nome":nome,
+        "guilda":guilda,
+        "profissão":profissao,
+        "Situação Economica":situacaoEconomica,
+        "Cor primária": corPrincipal,
+        "Comprovante":comprovante
+    }]
+    flash('Solicitação enviada com sucesso. Aguarde o retorno.')
+    with open('solicitacoesCadastro.json') as solicitacoesCadastro:
+        arquivos=json.load(solicitacoesCadastro)
+    arquivo=arquivos+user
+    with open('solicitacoesCadastro.json', 'w') as responder:
+        json.dumps(arquivo, arquivos, indent=4)
+        return render_template("filiacao.html")
+
+       
+
+    
 
 
 if __name__=="__main__":
